6a0e1048992dba5a5f1151464d4f711ca78d682d
unknown <a@gaoy.xyz>
2019-03-18 10:42:29 +0800

    a

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from mmodel import get_module
 
 if __name__ == "__main__":
 
-    torch.backends.cudnn.benchmark = True
+    # torch.backends.cudnn.benchmark = True
     torch.cuda.empty_cache()
     _, A = get_module("DANN")
     A.train_module()
