6cbedd6b238e76ac09ef7c7b5a81f9837c3a230d
Meeio <meeio@outlook.com>
2019-03-19 17:23:43 +0800

    a

--- mmodel/Finetune/model.py
+++ mmodel/Finetune/model.py
@@ -38,7 +38,8 @@ class Finetune(DAModule):
 
     def _regist_networks(self):
 
-        if True:
+        # aa
+        if False:
             from .networks.resnet50 import ResFc, ResClassifer
             F = ResFc()
             C = ResClassifer(class_num=31)
