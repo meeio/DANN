b0675ffa78a180f5b9e0124f64249d1f7d2edeee
unknown <a@gaoy.xyz>
2019-03-21 22:29:03 +0800

    a

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from mmodel import get_module
 if __name__ == "__main__":
 
     torch.backends.cudnn.benchmark = True
-    # name = input('model name:')
-    name = 'open'
+    name = input('model name:')
+    # name = 'open'
     _, A = get_module(name)
     A.train_module()
