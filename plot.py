3dfa66c67425ab18aad7f39bbb25a3afecdb52d6
Meeio <meeio@outlook.com>
2019-03-27 14:12:35 +0800

    a

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+
 from scipy import interpolate
 
 from matplotlib.font_manager import FontProperties
@@ -61,7 +61,7 @@ from mtrain.watcher import parse_losses_record, parse_watcher_dict
 # ================    ===============================
 
 
-def curve_graph(smooth_ration=2000, **kwargs):
+def curve_graph(smooth_ration=10, **kwargs):
 
     for name, records in kwargs.items():
 
@@ -69,11 +69,16 @@ def curve_graph(smooth_ration=2000, **kwargs):
         x = [records[0] * i for i in range(len(y))]
         data_count = len(y)
 
-        # x_smooth = np.linspace(min(x), max(x), data_count * smooth_ration)
-        # y_smooth = interpolate.spline(x, y, x_smooth)
+        # z1 = np.polyfit(x, y, 10) # 用3次多项式拟合
+        # p1 = np.poly1d(z1)
+
+        # yvals=p1(x) 
+        # 也可以使用yvals=np.polyval(z1,x)
+        x_smooth = np.linspace(min(x), max(x), data_count * smooth_ration)
+        y_smooth = interpolate.spline(x, y, x_smooth)
 
         # tck = interpolate.spline(x, y)
-        plt.plot(x, y, "-H", label=name)
+        plt.plot(x_smooth, y_smooth, "-", label=name, markevery=20)
 
     plt.legend(loc="best")
     plt.title("A10 to W10+10")
@@ -85,29 +90,17 @@ def for_accu(file):
     losses = parse_losses_record(record_dic)
     return losses["valid_accu"]
 
+def for_bias(file):
+    record_dic = parse_watcher_dict(file)
+    losses = parse_losses_record(record_dic)
+    return losses["bias"]
 
-a = for_accu(r"RECORDS\OPENBB_0325_1328.json")
-print(len(a[1]))
 
-import random
-
-random.seed(6110)
-b = list()
-for idx, y in enumerate(a[1]):
-    if idx > 25:
-        y = y - random.random() * (idx - 25) * 0.7
-    b.append(y)
-
-print(b)
-
-c = [a[0], b]
 
 accu = {
-    # "Back Prop": for_accu(r"RECORDS\OPENDP_0325_1153.json"),
-    "Thredhold Back Prop / bad": c,
-    "Thredhold Back Prop / good": for_accu(
-        r"RECORDS\OPENBB_0325_2153.json"
-    ),
+    "10 class": for_bias(r"RECORDS\OPENBBTEST_0326_0108.A10.json"),
+    "20 class": for_bias(r"RECORDS\OPENBBTEST_0326_0109.A20.json"),
+    "30 class": for_bias(r"RECORDS\OPENBBTEST_0326_0110.A31.json"),
 }
 
 
