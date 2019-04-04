f97d24c1906fe11db404595321239b6ac6f81873
Meeio <meeio@outlook.com>
2019-04-05 03:04:12 +0800

    a

--- mmodel/openset_drop/params.py
+++ mmodel/openset_drop/params.py
@@ -2,7 +2,7 @@ from ..basic_params import parser
 
 
 def get_params():
-    parser.add_argument("--dylr_alpht", type=float, default=10)
+    parser.add_argument("--dylr_alpht", type=float, default=20)
     parser.add_argument("--dylr_center", type=float, default=0.15)
     parser.add_argument("--dylr_high", type=float, default=0.01)
     parser.add_argument("--task_ajust_step", type=int, default=300)
